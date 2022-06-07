ebe81a23920984ec6db931530886f2d3202e18be
Gerson Salvador <gerson.salvador@outlook.com>
2022-06-07 15:21:56 -0300

    refac: refactor

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,38 @@
 class ChartCanvas {
 
-  constructor(elId, values, settings) {
+  constructor(elId, settings) {
 
-    this.PI = Math.PI
+    this.setConstants()
     this.handleSettings(settings, values)
     this.handleCanvasEl(elId)
     this.handleLineHeight()
     this.handleRaduis()
 
-    values.forEach((v, index) => this.drawCircle(v, index))
+  }
 
+  setConstants() {
+
+    this.PI = Math.PI
+    this.degree = 2 * this.PI / 360
+    this.defaultSettings = {
+      width: 400,
+      height: 400,
+      margin: [20, 20, 20, 20],
+      maxValue: 100,
+      maxDegree: 360 * .85,
+      fontFamily: 'sans-serif'
+    }
+
+  }
+
+  handleSettings(settings, values) {
+
+    const times = values.length
+    const { defaultSettings } = this
+
+    this.settings = { ...defaultSettings, ...settings, times }
+    this.handleXYAxis()
+    this.calcBaseValue()
   }
 
   handleColor(color) {
@@ -33,118 +56,6 @@ class ChartCanvas {
     }
 
     return color || `hsl(${hue}, 45%, 80%)`
-
-  }
-
-  calcDrgree(num) {
-
-    const { baseValue, maxDegree, } = this.settings
-    const value = num * baseValue
-    const finalValue = value > maxDegree ? maxDegree : value
-    const degree = (((finalValue / 180) + 1.5) * this.PI)
-
-    return { degree, finalValue }
-
-  }
-
-  calcRotateStep(radius, lineWidth) {
-
-    const { PI, degree } = this
-    const degreePx = (PI / 180) * radius
-    const ratio = lineWidth / degreePx
-
-    return ratio / degree / 6500
-
-  }
-
-  drawTextAlongArc(ctx, str, centerX, centerY, radius, finalValue, lineWidth) {
-
-    const { degree } = this
-    const rotate = this.calcRotateStep(radius, lineWidth)
-    const len = str.length
-    let s
-
-    ctx.save();
-    ctx.translate(centerX, centerY);
-    ctx.font = `${(lineWidth)}px monospace`;
-    ctx.rotate((degree * finalValue) + rotate * 1);
-
-    for (var n = 0; n < len; n++) {
-      s = str[n];
-      ctx.rotate(rotate);
-      ctx.save();
-      ctx.translate(0, -1 * radius);
-      ctx.fillText(s, 0, 0);
-      ctx.restore();
-    }
-
-    ctx.restore();
-  }
-
-  drawCircle({ color, value, label }, index) {
-
-    const { ctx, radius, } = this
-    const { lineWidth, margin, value: { prefix, sulfix } } = this.settings
-    const { degree, finalValue } = this.calcDrgree(value)
-
-    ctx.beginPath();
-    ctx.strokeStyle = color || this.handleColor();
-    ctx.lineWidth = lineWidth;
-    ctx.arc(this.x, this.y, radius[index], 1.5 * this.PI, degree);
-    ctx.stroke();
-
-    //label
-    ctx.font = `${(lineWidth)}px Arial`;
-    ctx.textAlign = 'end'
-    ctx.textBaseline = 'middle'
-    ctx.fillText(label, this.y - 4, margin[0] + ((index + 1) * (lineWidth + 1)) - (lineWidth * .43));
-
-    //value
-    ctx.font = `${(lineWidth * .75)}px Arial`;
-    this.drawTextAlongArc(
-      ctx,
-      `${prefix}${value}${sulfix}`,
-      this.x,
-      this.y,
-      radius[index],
-      finalValue,
-      lineWidth * .5
-    )
-
-  }
-
-  calcEndOfArc(degree, radius) {
-
-    const x = this.x + Math.cos(degree) * radius;
-    const y = this.y + Math.sin(degree) * radius;
-
-    return [x, y]
-  }
-
-  handleSettings(settings, values) {
-
-    const times = values.length
-
-    const defaultSettings = {
-      width: 400,
-      height: 400,
-      margin: [20, 20, 20, 20],
-      maxValue: 100,
-      maxDegree: 360 * .85,
-      times,
-    }
-
-    this.settings = { ...defaultSettings, ...settings }
-    this.degree = 2 * Math.PI / 360
-    this.handleXYAxis()
-    this.calcBaseValue()
-  }
-
-  calcBaseValue() {
-
-    const base = this.settings.maxDegree / this.settings.maxValue
-
-    this.settings.baseValue = base
 
   }
 
@@ -203,6 +114,105 @@ class ChartCanvas {
     const lineWidth = halfRadius / times
     const limit = halfRadius * .2
     this.settings.lineWidth = lineWidth > limit ? limit : lineWidth
+
+  }
+
+  calcDrgree(num) {
+
+    const { baseValue, maxDegree, } = this.settings
+    const value = num * baseValue
+    const finalValue = value > maxDegree ? maxDegree : value
+    const degree = (((finalValue / 180) + 1.5) * this.PI)
+
+    return { degree, finalValue }
+
+  }
+
+  calcRotateStep(radius, lineWidth) {
+
+    const { PI, degree } = this
+    const degreePx = (PI / 180) * radius
+    const ratio = lineWidth / degreePx
+
+    return ratio / degree / 6500
+
+  }
+
+  calcEndOfArc(degree, radius) {
+
+    const x = this.x + Math.cos(degree) * radius;
+    const y = this.y + Math.sin(degree) * radius;
+
+    return [x, y]
+  }
+
+  calcBaseValue() {
+
+    const base = this.settings.maxDegree / this.settings.maxValue
+
+    this.settings.baseValue = base
+
+  }
+
+  drawTextAlongArc(ctx, str, centerX, centerY, radius, finalValue, lineWidth) {
+
+    const { degree } = this
+    const rotate = this.calcRotateStep(radius, lineWidth)
+    const len = str.length
+    let s
+
+    ctx.save();
+    ctx.translate(centerX, centerY);
+    ctx.font = `${(lineWidth)}px monospace`;
+    ctx.rotate((degree * finalValue) + rotate);
+
+    for (var n = 0; n < len; n++) {
+      s = str[n];
+      ctx.rotate(rotate);
+      ctx.save();
+      ctx.translate(0, -1 * radius);
+      ctx.fillText(s, 0, 0);
+      ctx.restore();
+    }
+
+    ctx.restore();
+  }
+
+  drawCircle({ color, value, label }, index) {
+
+    const { ctx, radius, } = this
+    const { lineWidth, margin, value: { prefix, sulfix }, fontFamily } = this.settings
+    const { degree, finalValue } = this.calcDrgree(value)
+
+    ctx.beginPath();
+    ctx.strokeStyle = color || this.handleColor();
+    ctx.lineWidth = lineWidth;
+    ctx.arc(this.x, this.y, radius[index], 1.5 * this.PI, degree);
+    ctx.stroke();
+
+    //label
+    ctx.font = `${(lineWidth)}px ${fontFamily}`;
+    ctx.textAlign = 'end'
+    ctx.textBaseline = 'middle'
+    ctx.fillText(label, this.y - 4, margin[0] + ((index + 1) * (lineWidth + 1)) - (lineWidth * .43));
+
+    //value
+    ctx.font = `${(lineWidth * .75)}px ${fontFamily}`;
+    this.drawTextAlongArc(
+      ctx,
+      `${prefix}${value}${sulfix}`,
+      this.x,
+      this.y,
+      radius[index],
+      finalValue,
+      lineWidth * .5
+    )
+
+  }
+
+  plotData(values) {
+
+    values.forEach((v, index) => this.drawCircle(v, index))
 
   }
 
